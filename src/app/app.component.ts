5c7e29ba5507f2556261c488bb2681ba07350259
Hans Brems <hans.brems@longville-software.be>
2024-03-08 21:47:02 +0100

    Improve table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
+import { ContainerComponent } from './shared/components/layout/container/container.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ContainerComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
