9b3576628e1e82e200dbabf1dc2d038995816f5b
Hans Brems <hans.brems@longville-software.be>
2024-12-04 21:42:21 +0100

    More experimenting with state services

--- src/app/orders/list/list.page.ts
+++ src/app/orders/list/list.page.ts
@@ -1,14 +1,7 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
-
-import { StackComponent } from '~/shared/components/layout/stack/stack.component';
 
 import { OrderDetailsState } from './order-details.state';
 import { OrdersState } from './orders.state';
@@ -18,19 +11,9 @@ import { OrdersState } from './orders.state';
   templateUrl: './list.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [OrdersState, OrderDetailsState],
-  imports: [FormsModule, ButtonModule, DropdownModule, StackComponent],
+  imports: [FormsModule, ButtonModule, DropdownModule],
 })
 export class ListPage {
-  private readonly ordersState = inject(OrdersState);
-  private readonly orderDetailsState = inject(OrderDetailsState);
-
-  orders = this.ordersState.orders;
-  orderDetails = this.orderDetailsState.orderDetails;
-  selectedOrderId = signal<number | null>(
-    this.orderDetailsState.selectedOrderId(),
-  );
-
-  loadOrderDetails() {
-    this.orderDetailsState.selectOrder(this.selectedOrderId());
-  }
+  readonly ordersState = inject(OrdersState);
+  readonly orderDetailsState = inject(OrderDetailsState);
 }
--- src/app/orders/list/order-details.state.ts
+++ src/app/orders/list/order-details.state.ts
@@ -1,7 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { derivedAsync } from 'ngxtension/derived-async';
-import { Subject, filter, switchMap } from 'rxjs';
+import { Subject, filter, switchMap, withLatestFrom } from 'rxjs';
 
 import { OrderService } from '../shared/services/order.service';
 
@@ -9,31 +8,27 @@ import { OrderService } from '../shared/services/order.service';
 export class OrderDetailsState {
   private readonly orderService = inject(OrderService);
 
+  private readonly fetchTrigger$ = new Subject<void>();
   private readonly selectedOrderId$ = new Subject<number | null>();
-  selectedOrderId = toSignal(this.selectedOrderId$, {
+
+  readonly selectedOrderId = toSignal(this.selectedOrderId$, {
     initialValue: null,
   });
 
   readonly orderDetails = toSignal(
-    this.selectedOrderId$.pipe(
-      filter((id) => id !== null),
-      switchMap((id) => this.orderService.getOrder(id)),
+    this.fetchTrigger$.pipe(
+      withLatestFrom(this.selectedOrderId$),
+      filter(([, id]) => id !== null),
+      switchMap(([, id]) => this.orderService.getOrder(id!)),
     ),
     { initialValue: null },
   );
 
-  // The same but with derivedAsync.
-  // This will only trigger a refetch if the order ID is different from the last emitted value.
-  // readonly orderDetails = derivedAsync(
-  //   () => {
-  //     const id = this.selectedOrderId();
-  //     return id === null ? of(null) : this.orderService.getOrder(id);
-  //   },
-  //   { initialValue: null, requireSync: true },
-  // );
+  fetch() {
+    this.fetchTrigger$.next();
+  }
 
   selectOrder(orderId: number | null) {
-    const randomId = Math.floor(Math.random() * 5);
-    this.selectedOrderId$.next(randomId);
+    this.selectedOrderId$.next(orderId);
   }
 }
