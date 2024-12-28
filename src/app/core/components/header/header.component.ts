8227b51163b497e8a1d272df724f0869af1ff962
Hans Brems <hans.brems@longville-software.be>
2024-12-28 23:52:44 +0100

    Refactoring

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,11 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  OnInit,
+  computed,
   inject,
-  signal,
 } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
-import { NavLink } from '~/core/models/configuration/nav-link';
 import { ConfigurationService } from '~/core/services/configuration.service';
 import { NavBarComponent } from '~/shared/components/navigation/nav-bar/nav-bar.component';
 import { NavLinkComponent } from '~/shared/components/navigation/nav-link/nav-link.component';
@@ -18,13 +17,12 @@ import { NavLinkComponent } from '~/shared/components/navigation/nav-link/nav-li
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   private readonly configurationService = inject(ConfigurationService);
 
-  links = signal<NavLink[]>([]);
+  config = toSignal(this.configurationService.getConfiguration(), {
+    initialValue: null,
+  });
 
-  async ngOnInit(): Promise<void> {
-    const configuration = await this.configurationService.getConfiguration();
-    this.links.set(configuration.header.links);
-  }
+  links = computed(() => this.config()?.header.links ?? []);
 }
--- src/app/core/services/configuration.service.ts
+++ src/app/core/services/configuration.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { AppConfiguration } from '../models/configuration/app-configuration';
 
@@ -10,9 +10,7 @@ import { AppConfiguration } from '../models/configuration/app-configuration';
 export class ConfigurationService {
   private readonly http = inject(HttpClient);
 
-  getConfiguration(): Promise<AppConfiguration> {
-    return firstValueFrom(
-      this.http.get<AppConfiguration>('/assets/config/app-config.json'),
-    );
+  getConfiguration(): Observable<AppConfiguration> {
+    return this.http.get<AppConfiguration>('/assets/config/app-config.json');
   }
 }
